b68b30004f3a0e78b3b7feea09c235efe2dfcaf4
Anshul-Dishoriya <anshuldishoriya@gmail.com>
2023-10-06 15:26:33 +0530

    Added icon field to category model

--- chatapp/server/models.py
+++ chatapp/server/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here.
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
@@ -13,20 +17,28 @@ class Category(models.Model):
 
 class Server(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="server_category"
+    )
     description = models.CharField(max_length=500, blank=True, null=True)
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self) -> str:
         return f"{self.name}-{self.id}"
-    
+
 
 class Channel(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
     topic = models.CharField(max_length=100)
-    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channel_server"
+    )
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
@@ -34,4 +46,3 @@ class Channel(models.Model):
 
     def __str__(self):
         return self.name
-    
