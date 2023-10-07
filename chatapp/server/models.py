78855072b3e552a77ba0c48828134d7ae4600930
Anshul-Dishoriya <anshuldishoriya@gmail.com>
2023-10-07 16:25:49 +0530

    Added banner and icon fields in channels model

--- chatapp/server/models.py
+++ chatapp/server/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+from .validators import validate_icon_image_size, validate_image_file_extension
+
+
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icons/{filename}"
+
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
 
 
 def category_icon_upload_path(instance, filename):
@@ -10,6 +21,42 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_file(sender, instance, **kwargs):
+        """Delete associated files when a Category instance is deleted.
+
+        This function is intended to be used as a signal handler for the pre_delete
+        signal of the 'Category' model. It deletes the associated 'icon' file, if it
+        exists, when a Category instance is about to be deleted.
+
+        Args:
+            sender: The sender of the signal, which is the 'Category' model.
+            instance: The instance of the 'Category' model about to be deleted.
+            **kwargs: Additional keyword arguments provided by the signal.
+
+        Returns:
+            None
+
+        Raises:
+            None
+
+        Note:
+            This function assumes that the 'Category' model has a field named 'icon'
+            that represents the associated file to be deleted.
+        """
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self) -> str:
         return self.name
@@ -39,10 +86,56 @@ class Channel(models.Model):
     server = models.ForeignKey(
         Server, on_delete=models.CASCADE, related_name="channel_server"
     )
+    banner = models.ImageField(
+        upload_to=server_banner_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_image_file_extension, validate_icon_image_size],
+    )
+    icon = models.ImageField(
+        upload_to=server_icon_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_image_file_extension, validate_icon_image_size],
+    )
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel, self).save(*args, **kwargs)
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_file(sender, instance, **kwargs):
+        """Delete associated files when a Category instance is deleted.
+
+        This function is intended to be used as a signal handler for the pre_delete
+        signal of the 'Category' model. It deletes the associated 'icon' file, if it
+        exists, when a Category instance is about to be deleted.
+
+        Args:
+            sender: The sender of the signal, which is the 'Category' model.
+            instance: The instance of the 'Category' model about to be deleted.
+            **kwargs: Additional keyword arguments provided by the signal.
+
+        Returns:
+            None
+
+        Raises:
+            None
+
+        Note:
+            This function assumes that the 'Category' model has a field named 'icon'
+            that represents the associated file to be deleted.
+        """
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
--- chatapp/server/views.py
+++ chatapp/server/views.py
@@ -12,6 +12,7 @@ from .schema import server_list_docs
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
+
     @server_list_docs
     def list(self, request):
         """List and filter Server objects based on query parameters.
